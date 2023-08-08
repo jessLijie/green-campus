dfaf0232283e56ac9163bcf2eb97da014bf920c8
Jingyi <jingyi012@gmail.com>
2023-08-08 10:25:35 +0800

    update

--- connectdb.php
+++ connectdb.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("localhost", "root", "jkty12138", "greenify");
+    $con = mysqli_connect("localhost", "greenify", "1234", "greenify");
 
     if(!$con){
         die('Could not connect: '.mysqli_connect_error());
