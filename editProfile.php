ce5e4233b73ee72cbcd4c23522b5eed0699e99d0
MJ <chaijing13691@gmail.com>
2023-08-24 18:58:40 +0800

    up

--- editProfile.php
+++ editProfile.php
@@ -27,8 +27,11 @@
 </head>
 
 <body>
-    <?php 
+    <?php
     include('connectdb.php');
+
+    $statusMsg = '';
+    $targetDir = "images/profileImg/";
 
     if (isset($_SESSION['username'])) {
         $username = $_SESSION['username'];
@@ -37,18 +40,43 @@
 
     }
 
-    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    if (isset($_POST['editProfile'])) {
         $newUsername = $_POST['newUsername'];
         $newEmail = $_POST['newEmail'];
 
         $verify_query = mysqli_query($con, "SELECT email FROM users WHERE email='$newEmail'");
 
         if (mysqli_num_rows($verify_query) != 0 && $newEmail != $row['email']) {
-
         } else {
+
             $updateQuery = "UPDATE users SET username='$newUsername', email='$newEmail' WHERE username='$username'";
             mysqli_query($con, $updateQuery);
             $_SESSION['username'] = $newUsername;
+
+            if ($_FILES["file"]["name"]) {
+                $fileName = basename($_FILES["file"]["name"]);
+                $targetFilePath = $targetDir . $fileName;
+                $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
+        
+                $allowTypes = array('jpg', 'png', 'jpeg', 'gif');
+                if (in_array($fileType, $allowTypes)) {
+                    if (move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)) {
+                        $insert = mysqli_query($con, "UPDATE users SET userImage = ('" . $fileName . "') WHERE email = '$newEmail'");
+                        if ($insert) {
+                            $statusMsg = "The file " . $fileName . " has been uploaded successfully.";
+                        } else {
+                            $statusMsg = "File upload failed, please try again.";
+                        }
+                    } else {
+                        $statusMsg = "Sorry, there was an error uploading your file.";
+                    }
+                } else {
+                    $statusMsg = 'Sorry, only JPG, JPEG, PNG, & GIF files are allowed to upload.';
+                }
+            }
+
+            echo $statusMsg;
+
             header('Location: profile.php');
             exit();
         }
@@ -61,10 +89,16 @@
     <div class="row g-3 align-items-center"
         style="margin: 6% 30% 6% 30%; padding: 2%; border: 1px solid grey; border-radius: 12px;">
         <h1 style="margin-top: 0">Edit Profile</h1>
-        <form method="POST" action="">
+        <form method="POST" action="" enctype="multipart/form-data">
             <div class="profile-container">
                 <img src="images/profileImg/profile.jpg" alt="Avatar" class="round" width="300" height="400">
             </div>
+
+            <div class="mb-3">
+                <label for="file" class="form-label">Profile picture</label>
+                <input class="form-control" type="file" name="file" id="file">
+            </div>
+
             <div class="mb-3">
                 <label for="newUsername" class="form-label">Username</label>
                 <div>
