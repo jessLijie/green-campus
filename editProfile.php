05f8324152c6793ee48363997c21be6eda17a840
MJ <chaijing13691@gmail.com>
2023-08-26 17:30:28 +0800

    update css

--- editProfile.php
+++ editProfile.php
@@ -10,18 +10,31 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <style>
+        body {
+            background-image: linear-gradient(to right, #8a64d6, #0cead9)
+        }
+
         img.round {
             object-fit: cover;
             border-radius: 50%;
             height: 200px;
             width: 200px;
             box-sizing: content-box;
+            box-shadow: 3px 3px 10px 2px #888888;
         }
 
         .profile-container {
             padding: 1%;
             margin-bottom: 4%;
             text-align: center;
+        }
+
+        #cointainer {
+            margin: 6% 30% 6% 30%;
+            padding: 2%;
+            border-radius: 12px;
+            background-color: white;
+            box-shadow: 3px 3px 20px 2px #888888;
         }
     </style>
 </head>
@@ -45,9 +58,9 @@
         $newEmail = $_POST['newEmail'];
 
         $verify_email = mysqli_query($con, "SELECT email FROM users WHERE email='$newEmail'");
-        $verify_username=mysqli_query($con, "SELECT username FROM users WHERE username='$newUsername'");
+        $verify_username = mysqli_query($con, "SELECT username FROM users WHERE username='$newUsername'");
 
-        
+
         if ((mysqli_num_rows($verify_email) != 0 && $newEmail != $row['email']) || (mysqli_num_rows($verify_username) != 0 && $newUsername != $row['username'])) {
         } else {
 
@@ -88,13 +101,15 @@
 
     <?php include('header.php'); ?>
 
-    <div class="row g-3 align-items-center"
-        style="margin: 6% 30% 6% 30%; padding: 2%; border: 1px solid grey; border-radius: 12px;">
-        <h1 style="margin-top: 0">Edit Profile</h1>
+    <div class="row g-3 align-items-center" id="cointainer">
+        <h2 style="margin-top: 0">Edit Profile</h2>
         <form method="POST" action="" enctype="multipart/form-data">
             <div class="profile-container">
-                <img src="images/<?php if(!$row['userImage']){echo 'defaultprofile.png';}else{echo 'profileImg/' .$row['userImage'];}?>" alt="Avatar" class="round" width="300"
-                    height="400">
+                <img src="images/<?php if (!$row['userImage']) {
+                    echo 'defaultprofile.png';
+                } else {
+                    echo 'profileImg/' . $row['userImage'];
+                } ?>" alt="Avatar" class="round" width="300" height="400">
             </div>
 
             <div class="mb-3">
@@ -119,6 +134,7 @@
             </div>
 
             <div class="d-grid gap-2 d-md-block">
+                <a href="profile.php"><button type="button" class="btn btn-outline-primary">Cancel</button></a>
                 <input type="submit" class="btn btn-primary" name="editProfile" value="Submit">
             </div>
         </form>
--- profile.php
+++ profile.php
@@ -10,18 +10,36 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <style>
+        body {
+            background-image: linear-gradient(to right, #8a64d6, #0cead9)
+        }
+
+        input[type=text],
+        input[type=email] {
+            background-color: #EEEEEE;
+        }
+
         img.round {
             object-fit: cover;
             border-radius: 50%;
             height: 200px;
             width: 200px;
             box-sizing: content-box;
+            box-shadow: 3px 3px 10px 2px #888888;
         }
 
         .profile-container {
             padding: 1%;
             margin-bottom: 4%;
             text-align: center;
+        }
+
+        #cointainer {
+            margin: 6% 30% 6% 30%;
+            padding: 2%;
+            border-radius: 12px;
+            background-color: white;
+            box-shadow: 3px 3px 20px 2px #888888;
         }
     </style>
 </head>
@@ -38,17 +56,20 @@
 
     ?>
 
-    <div class="row g-3 align-items-center"
-        style="margin: 6% 30% 6% 30%; padding: 2%; border: 1px solid grey; border-radius: 12px;">
-        <h1 style="margin-top: 0">Profile</h1>
+    <div class="row g-3 align-items-center" id="cointainer">
+        <h2 style="margin-top: 0">Profile</h2>
         <form>
             <div class="profile-container">
-                <img src="images/<?php if(!$row['userImage']){echo 'defaultprofile.png';}else{echo 'profileImg/' .$row['userImage'];}?>" alt="Avatar" class="round" width="300" height="400">
+                <img src="images/<?php if (!$row['userImage']) {
+                    echo 'defaultprofile.png';
+                } else {
+                    echo 'profileImg/' . $row['userImage'];
+                } ?>" alt="Avatar" class="round" width="300" height="400">
             </div>
             <div class="mb-3">
                 <label for="username" class="form-label">Username</label>
                 <div>
-                    <input type="username" readonly class="form-control" id="username"
+                    <input type="text" readonly class="form-control" id="username"
                         value="<?php echo $row['username']; ?>">
                 </div>
             </div>
@@ -61,7 +82,7 @@
             </div>
 
             <div class="d-grid gap-2 d-md-block">
-                <a href="editProfile.php"><button type="button" class="btn btn-primary">Edit Profile</button></a>                
+                <a href="editProfile.php"><button type="button" class="btn btn-primary">Edit Profile</button></a>
             </div>
         </form>
 
