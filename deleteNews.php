cf959ac5950a83c15f77c489d9be9eabe44033cc
MJ <chaijing13691@gmail.com>
2023-08-29 21:49:06 +0800

    update upload image funtionality

--- deleteNews.php
+++ deleteNews.php
@@ -6,9 +6,14 @@ include("connectdb.php");
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
 
+    $query = "SELECT * FROM newsfeed WHERE id = $id";
+    $result = mysqli_query($con, $query);
+    $row = mysqli_fetch_array($result);
+
     $sql = "DELETE FROM newsfeed WHERE id = '$id'";
 
     if (mysqli_query($con, $sql)) {
+        unlink('images/newsImg/' .$row['image_url']);
         echo "News deleted successfully.";
         header('Location: adminManage.php');
         exit;
