439698455491769b333192f38ebf463afab4853c
Jingyi <jingyi012@gmail.com>
2024-03-26 12:53:01 +0800

    update

--- connectdb.php
+++ connectdb.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("localhost", "root", "jkty12138", "greenify");
+    $con = mysqli_connect("localhost", "root", "", "greenify");
 
     if(!$con){
         die('Could not connect: '.mysqli_connect_error());
--- database/createdb.php
+++ database/createdb.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost", "root", "jkty12138");
+$con = mysqli_connect("localhost", "root", "");
 
 if(!$con){
     die('Could not connect: '.mysqli_connect_error());
